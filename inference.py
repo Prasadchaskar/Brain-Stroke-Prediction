e862626c97201a1d92481160f1db9e8240af9910
Chaskar Prasad Bhausaheb <72083319+Prasadchaskar@users.noreply.github.com>
2021-09-20 18:34:27 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,20 +1,16 @@
 import pickle
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-scalr = StandardScaler()
 model = pickle.load(open('stroke.pkl', 'rb'))
+scaler = pickle.load(open('scalar.pkl', 'rb'))
 class_names = ['Yes','No']
-
 def predict(df):
-        df = df[['gender', 'age', 'hypertension', 'heart_disease', 'ever_married',
-        'work_type', 'Residence_type', 'avg_glucose_level', 'bmi',
-        'smoking_status']]
-
+        df = df[['gender', 'age', 'hypertension', 'heart_disease', 'ever_married','work_type', 'Residence_type', 'avg_glucose_level', 'bmi','smoking_status']]
         numpy_array = df.to_numpy()
-        numpy_array = scalr.fit_transform(numpy_array)
         # Predict
+        numpy_array = scaler.transform(numpy_array)
         predictions = model.predict(numpy_array)
+        
         #return str(predictions)
         output = [class_names[class_predicted] for class_predicted in predictions]
         return output
