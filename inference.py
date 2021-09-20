f904f28885588cdf63935b51cafdc98b98b51bc4
Chaskar Prasad Bhausaheb <72083319+Prasadchaskar@users.noreply.github.com>
2021-09-20 17:46:41 +0530

    Add files via upload

---
+++ inference.py
@@ -0,0 +1,20 @@
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+scalr = StandardScaler()
+model = pickle.load(open('stroke.pkl', 'rb'))
+class_names = ['Yes','No']
+
+def predict(df):
+        df = df[['gender', 'age', 'hypertension', 'heart_disease', 'ever_married',
+        'work_type', 'Residence_type', 'avg_glucose_level', 'bmi',
+        'smoking_status']]
+
+        numpy_array = df.to_numpy()
+        numpy_array = scalr.transform()
+        # Predict
+        predictions = model.predict(numpy_array)
+        #return str(predictions)
+        output = [class_names[class_predicted] for class_predicted in predictions]
+        return output
